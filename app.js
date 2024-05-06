fd0e46365907d87bf585089eeeaa71386eac6850
amitay23 <119876739+amitay23@users.noreply.github.com>
2024-05-06 19:30:37 +0300

    a basic server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+app.listen(PORT, ()=>{
+console.log("app is listening in port " +PORT)
+})
