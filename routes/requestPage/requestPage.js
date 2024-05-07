dd136698aaeafe7f62cd611243a19a96b4ef6d32
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-07 19:50:55 +0300

    fix bug

--- routes/requestPage/requestPage.js
+++ routes/requestPage/requestPage.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
+const express = require('express');
 const router = Router();
+const path = require('path');
 
 router.get('/',(req,res) => {
     const file = path.join(__dirname = '../../../public/request/request.html');
