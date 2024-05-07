0065ff6573e068c0f53f803ba67319a1c76bd996
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-07 19:48:14 +0300

    request page route

--- routes/requestPage/requestPage.js
+++ routes/requestPage/requestPage.js
@@ -1,4 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 
+router.get('/',(req,res) => {
+    const file = path.join(__dirname = '../../../public/request/request.html');
+    res.sendFile(file);
+})
+
 module.exports = router;
