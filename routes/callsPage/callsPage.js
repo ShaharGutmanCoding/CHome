c006d12bd78960e6c889d7d782934ed424c2bd01
Shahar Gutman <shahargut1@gmail.com>
2024-05-16 19:42:45 +0300

    fixed route for calls page

--- routes/callsPage/callsPage.js
+++ routes/callsPage/callsPage.js
@@ -3,7 +3,7 @@ const router = Router();
 const path = require('path');
 
 router.get('/',(req,res) =>{
-    const file = path.join(__dirname + '../../../../public/callsPage.html');
+    const file = path.join(__dirname + '../../../public/callsPage.html');
     res.sendFile(file);
 })
 
