be8d6c35cf79d0c6d69011f81a57e0346ab78ba1
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-16 19:31:01 +0300

    add scheme

--- scheme/scheme.js
+++ scheme/scheme.js
@@ -1,7 +1,32 @@
 const mongoose = require('mongoose');
 
 const users = new mongoose.Scheme({
-    
+    name:{
+        type:String,
+        default:'No-typed',
+        require: true,
+    },
+
+    password:{
+        type:String,
+        default:'',
+        require: true,
+    },
+
+    email:{
+        type:String,
+        default:'',
+    },
+
+    id:{
+        type:Number,
+        default:0,
+    },
+
+    phoneNum:{
+        type:Number,
+        default:0,
+    },
 })
 
 module.exports = mongoose.model('users',users);
