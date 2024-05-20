781907a58cdce3989e8bcc67e65e00d2c49d9169
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-20 18:41:23 +0300

    adding ticket scheme & post for calls and register

--- routes/callsPage/callsPage.js
+++ routes/callsPage/callsPage.js
@@ -1,10 +1,20 @@
 const {Router} = require('express');
 const router = Router();
 const path = require('path');
+const ticket = require('../../scheme/ticket');
 
 router.get('/',(req,res) =>{
     const file = path.join(__dirname + '../../../public/callsPage.html');
     res.sendFile(file);
 })
 
+router.post('/newCall',async(req,res) => {
+    let category = req.body?.category;
+    let date = req.body?.date;
+    let description = req.body?.date;
+
+    if(!category && !date && !description){    
+        await ticket.create({category: category, date: date, description: description,})
+    }
+})
 module.exports = router;
