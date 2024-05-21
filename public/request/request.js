b39a4e1c73c0d74a2430163538c815db3f795f02
Noam134 <noameldan4@gmail.com>
2024-05-21 21:24:33 +0300

    Updated request

--- public/request/request.js
+++ public/request/request.js
@@ -1,4 +1,5 @@
 var categorySelect = document.getElementById("select_page")
+const form = document.getElementById('formSelect')
 
 let categorysObject = [
   {categoryName:"👩🏻‍🍼בייביסיטר ",valueId:"Babysitting",},
@@ -9,13 +10,58 @@ let categorysObject = [
   {categoryName:"⬅️אחר",valueId:"Other",},
 ]
 
+
+
 for(let i = 0; i < categorysObject.length; i++){
   const option = document.createElement("option")
   option.text=categorysObject[i].categoryName
   option.id = (categorysObject[i].valueId)
   categorySelect.appendChild(option)
 }
-document.createElement("option")
+console.log(form)
+form.addEventListener('submit', async(event) => {
+  event.preventDefault();
+  let category = document.getElementById('formSelect').value;
+  let year = document.getElementById('dateYear').value;
+  let month = document.getElementById('dateMonth').value;
+  let day = document.getElementById('dateDay').value;
+  let requestNote = document.getElementById('reqNotes').value;
+  ShowModal(201,year,category,requestNote);
+
+  // await fetch('/requestPage/newCall', {
+  //     method:'Post',
+  //     Credential:'include',
+  //     headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+  //     body:`category=${select_page}&year=${dateYear}&month=${dateMonth}&day=${dateDay}&requestNote=${reqNote}`
+  // })
+
+  // .then(()=> window.location.href = '/loginSystem/login')
+});
+function ShowModal(code, date,category,note){
+  let dateModal = document.getElementById('modalDate');
+  let categoryModal = document.getElementById('modalCategory');
+  let noteReqModal = document.getElementById('modalNoteReq');
+  if(code==200){
+    dateModal.text = date
+    categoryModal.text = category
+    noteReqModal.text = note
+    
+  }
+  else{
+    const alertDiv = document.createElement('div')
+    alertDiv.classList.add('alert alert-danger')
+    alertDiv.text("קרתה שגיאה,אנא נסה שוב ובדוק את הפרטים שהזנת")
+    let modalBody = document.getElementById('modalBody')
+    modalBody.appendChild(alertDiv)
+  }
+  
+  var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
+    keyboard: false
+  });
+  myModal.show();
+  
+}
+
 
 
 //Calander script (maybe delete)
