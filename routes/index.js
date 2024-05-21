56e8c7bbc8f10f126576fe092eabfa738a2f0c86
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-21 18:50:25 +0300

    adding redirect & header improvement

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const loginSystem = require('./loginSystem/loginSystem');
 const requestPage = require('./requestPage/requestPage');
 const callsPage = require('./callsPage/callsPage.js');
 
+router.get('/', (req, res) => {
+    res.redirect('/loginSystem/login');
+})
+
 router.use('/loginSystem', loginSystem);
 router.use('/requestPage', requestPage);
 router.use('/callsPage', callsPage);
