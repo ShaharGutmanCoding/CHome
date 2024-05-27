695b744857d5726a28123f409a1b1913f2187658
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-05-27 17:55:32 +0300

    home page fixing

--- routes/homePage/homePage.js
+++ routes/homePage/homePage.js
@@ -3,7 +3,7 @@ const router = Router();
 const path = require('path');
 
 router.get("/",(req,res)=>{
-    const file = path.join(__dirname + '../../../public/home.html');
+    const file = path.join(__dirname + '../../../public/homePage/home.html');
     res.sendFile(file);
 });
 
