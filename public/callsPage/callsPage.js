dc0d502fb1f56bd8f4ce029a8a661ecc1a8e9e18
Shahar Gutman <shahargut1@gmail.com>
2024-05-27 18:08:25 +0300

    g

--- public/callsPage/callsPage.js
+++ public/callsPage/callsPage.js
@@ -1,24 +1,51 @@
-
-let counter = document.getElementById("counter")
+let counter = document.getElementById("counter");
 let requestsContainer = document.getElementById("requestsContainer");
 
 function createCall(object) {
   let div = document.createElement("div");
   div.classList.add("Request");
 
-  // Create a span for the prescription
-  let prescription = document.createElement("span");
-  prescription.textContent = object.category;
-  prescription.style.display = "block"; 
-  prescription.style.textAlign = "center";
-  prescription.style.textDecoration = "underline"; 
-  prescription.style.fontWeight = "bold";
-  div.appendChild(prescription);
+  // Create a span for the category
+  let category = document.createElement("span");
+  category.textContent = object.category;
+  category.style.display = "block"; 
+  category.style.textAlign = "center";
+  category.style.textDecoration = "underline"; 
+  category.style.fontWeight = "bold";
+  div.appendChild(category);
 
   // Create a span for the description
   let description = document.createElement("span");
   description.textContent = object.description;
+  description.classList.add("description");
   div.appendChild(description);
+
+  // Check if the description is too long
+  const maxLength = 100; // maximum length before truncating
+  if (object.description.length > maxLength) {
+    let truncatedText = object.description.substring(0, maxLength) + "...";
+    description.textContent = truncatedText;
+
+    let readMoreDiv = document.createElement("div");
+    readMoreDiv.style.textAlign = "center";
+    readMoreDiv.style.marginTop = "5px"; // margin to separate button from text
+
+    let readMoreBtn = document.createElement("button");
+    readMoreBtn.textContent = "המשך קריאה";
+    readMoreBtn.classList.add("btn", "btn-link", "read-more");
+    readMoreBtn.onclick = function() {
+      if (description.textContent === truncatedText) {
+        description.textContent = object.description;
+        readMoreBtn.textContent = "קרא פחות";
+      } else {
+        description.textContent = truncatedText;
+        readMoreBtn.textContent = "המשך קריאה";
+      }
+    };
+
+    readMoreDiv.appendChild(readMoreBtn);
+    div.appendChild(readMoreDiv);
+  }
 
   // Create a span for the date
   let date = document.createElement("span");
@@ -32,9 +59,10 @@ function createCall(object) {
 }
 
 fetch('/callsPage/getCalls')
-.then(response=>response.json())
-.then(data=>{
-counter.innerHTML = "There are " + data.length + " requests";
-for(let i=0; i<data.length; i++ )
-createCall(data[i]);
-})
+  .then(response => response.json())
+  .then(data => {
+    counter.innerHTML = "There are " + data.length + " requests";
+    for (let i = 0; i < data.length; i++) {
+      createCall(data[i]);
+    }
+  });
