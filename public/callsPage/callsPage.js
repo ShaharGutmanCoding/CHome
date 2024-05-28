83e728cc8e34797371b7d3aea7c1d7baccc6b620
amitay23 <119876739+amitay23@users.noreply.github.com>
2024-05-28 11:49:11 +0300

    nothing

--- public/callsPage/callsPage.js
+++ public/callsPage/callsPage.js
@@ -1,3 +1,26 @@
+var categorySelect = document.getElementById("select_page")
+const form = document.getElementById('formSelect')
+
+let categorysObject = [
+  {categoryName:"👩🏻‍🍼בייביסיטר ",valueId:"Babysitting",},
+  {categoryName:"🛻הסעות",valueId:"Drives",},
+  {categoryName:"🛒קניות לבית",valueId:"Shopping",},
+  {categoryName:" 🐈‍⬛טיול לחיות מחמד",valueId:"PetWalk",},
+  {categoryName:"🍳 בישולים",valueId:"Cooking",},
+  {categoryName:"⬅️אחר",valueId:"Other",},
+]
+
+
+
+for(let i = 0; i < categorysObject.length; i++){
+  const option = document.createElement("option")
+  option.text=categorysObject[i].categoryName
+  option.id = (categorysObject[i].valueId)
+  categorySelect.appendChild(option)
+}
+
+
+
 
 let counter = document.getElementById("counter")
 let requestsContainer = document.getElementById("requestsContainer");
