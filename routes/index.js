89223bc4e319ab1f098694ab657af327baf68736
yairf1 <128710549+yairf1@users.noreply.github.com>
2024-06-03 19:10:38 +0300

    adding error page

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const loginSystem = require('./loginSystem/loginSystem');
 const requestPage = require('./requestPage/requestPage.js');
 const callsPage = require('./callsPage/callsPage.js');
 const homePage = require('./homePage/homePage');
-const profilePage = require('./profilePage/profilePage.js')
+const profilePage = require('./profilePage/profilePage.js');
+const error = require('./error/error.js');
 
 router.use('/loginSystem', loginSystem);
 router.use('/requestPage', requestPage);
 router.use('/callsPage', callsPage);
 router.use('/profilePage',profilePage);
+router.use('/error',error);
 router.use('/',homePage);
 
 module.exports = router;
