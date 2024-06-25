3dde0972b0eaf8fd4c041419925bb434df71b8d9
amitay23 <119876739+amitay23@users.noreply.github.com>
2024-06-25 15:01:00 +0300

    adding logout

--- public/header/header.js
+++ public/header/header.js
@@ -11,14 +11,23 @@ function getCookie(name) {
 
 let login = document.getElementById('login');
 let register = document.getElementById('register');
+let logout = document.getElementById('logout');
 console.log(getCookie('isLogged'));
 if (getCookie('isLogged')) {
     login.textContent = 'הפרופיל שלי';
+    logout.textContent= 'התנתקות';
     login.setAttribute('href', '/profilePage');
+    logout.setAttribute('href', '/loginSystem/login');
     register.textContent = "";
 } else {
     login.textContent = 'התחברות';
     login.setAttribute('href', '/loginSystem/login');
     register.textContent = 'הרשמה';
     register.setAttribute('href', '/loginSystem/register');
+}
+
+function logOut(){
+  document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "isLogged=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "firstName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
